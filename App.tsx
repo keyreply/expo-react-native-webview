836ebe06154e71f6c2ec4d9e5e632d70ad38a544
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-06-09 11:00:54 +0700

    putting things with native base

--- App.tsx
+++ App.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Alert, SafeAreaView, Modal } from 'react-native';
+import { Container, Header, Content, Fab, Icon, Left, Body, Button, Text, Title, View } from 'native-base';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Webview React Native</Title>
+          </Body>
+        </Header>
+        <Fab
+          direction="up"
+          containerStyle={{ }}
+          style={{ backgroundColor: '#5067FF' }}
+          position="bottomRight"
+          onPress={() => setModalVisible(true)}
+        >
+          <Icon name="md-chatbubbles" />
+        </Fab>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+        }}>
+          <Container>
+            <Header>
+              <Left>
+                <Button
+                  danger
+                  rounded
+                  transparent
+                  iconLeft
+                  onPress={() => setModalVisible(false)}
+                >
+                  <Icon name="arrow-back" />
+                </Button>
+              </Left>
+            </Header>
+            <Content>
+              <Text>Open up App.tsx to start working on your app!</Text>
+            </Content>
+          </Container>
+        </Modal>
+      </Container>
+    </SafeAreaView>
   );
 }
 
@@ -13,7 +57,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
