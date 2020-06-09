7764d66c2ffff6cfcfeb8206728078177bde6ddf
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-06-09 21:42:41 +0700

    safe area view removed

--- App.tsx
+++ App.tsx
@@ -6,49 +6,47 @@ export default function App() {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Container>
-        <Header>
-          <Body>
-            <Title>Webview React Native</Title>
-          </Body>
-        </Header>
-        <Fab
-          direction="up"
-          containerStyle={{ }}
-          style={{ backgroundColor: '#5067FF' }}
-          position="bottomRight"
-          onPress={() => setModalVisible(true)}
-        >
-          <Icon name="md-chatbubbles" />
-        </Fab>
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-        }}>
-          <Container>
-            <Header>
-              <Left>
-                <Button
-                  rounded
-                  transparent
-                  iconLeft
-                  onPress={() => setModalVisible(false)}
-                >
-                  <Icon name="arrow-back" />
-                </Button>
-              </Left>
-            </Header>
-            <Content>
-              <Text>Open up App.tsx to start working on your app!</Text>
-            </Content>
-          </Container>
-        </Modal>
-      </Container>
-    </SafeAreaView>
+    <Container>
+      <Header>
+        <Body>
+          <Title>Webview React Native</Title>
+        </Body>
+      </Header>
+      <Fab
+        direction="up"
+        containerStyle={{ }}
+        style={{ backgroundColor: '#5067FF' }}
+        position="bottomRight"
+        onPress={() => setModalVisible(true)}
+      >
+        <Icon name="md-chatbubbles" />
+      </Fab>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+      }}>
+        <Container>
+          <Header>
+            <Left>
+              <Button
+                rounded
+                transparent
+                iconLeft
+                onPress={() => setModalVisible(false)}
+              >
+                <Icon name="arrow-back" />
+              </Button>
+            </Left>
+          </Header>
+          <Content>
+            <Text>Open up App.tsx to start working on your app!</Text>
+          </Content>
+        </Container>
+      </Modal>
+    </Container>
   );
 }
 
