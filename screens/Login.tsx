35d50b38db5e858bcaa7581bda60bd983210de06
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-09-11 20:11:53 +0700

    login fixed

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -17,7 +17,6 @@ export default function Login({ navigation }: any) {
       const { data }: any = await axios.post('/', {
         username
       })
-      console.log(data);
       setLoading(false);
       AsyncStorage.setItem('token', data);
       navigation.navigate('Main');
