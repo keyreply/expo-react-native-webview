df178bbb1ac153a74a41811892aa55b278847f72
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-09-12 21:40:38 +0700

    useEffect font and notifications and permissions

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createAppContainer } from 'react-navigation';
+import * as Font from 'expo-font';
 
 import { SwitchNavigation } from './navigations';
 
 export default function App() {
+  const [ready, setReady] = useState(false);
   const Route = createAppContainer(SwitchNavigation);
+
+  useEffect(() => {
+    (async() => {
+      await Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
+      })
+      setReady(true);  
+    })();
+  })
   return (
+    ready &&
     <Route />
   );
 }
