c1e821bcb00c0e1631d14739fd879ebc27ee679f
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-10-08 21:45:57 +0700

    set modal on when receiving notification

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -209,7 +209,7 @@ export default function Main({ URI, navigation, SERVER }: { URI: string, navigat
 
     // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-
+      setModalVisible(true);
     });
 
     return () => {
